a47bae0ce431d875b0db6e21837b653825497b82
Gabriela Tomaz <gabrielatomaz1772@gmail.com>
2021-06-18 22:12:43 -0300

    11-06 computer lab exercises - shell script

--- 2021/1/18-06/exercise8.sh
+++ 2021/1/18-06/exercise8.sh
@@ -1,3 +1,5 @@
+# Gabriela Tomaz do Amaral Ribeiro
+
 # Crie um script que peça um mês e um ano do usuário 
 # e depois apresente um calendário do mês/ano pedido. 
 # Use o comando cal para lhe ajudar.
@@ -9,3 +11,5 @@ echo Digite o número de um ano:
 read year
 
 echo $(cal $month $year)
+
+# Gabriela Tomaz do Amaral Ribeiro
