8f1cb1f53ea1a7012399a201fc1a866272168996
Gabriela Tomaz <gabrielatomaz1772@gmail.com>
2022-06-19 20:40:29 -0300

    remove unused imports

--- 2022/1/12-06/computer-club/src/LoginController.java
+++ 2022/1/12-06/computer-club/src/LoginController.java
@@ -1,12 +1,9 @@
 import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 import context.StageContext;
-import entities.Post;
 import entities.User;
-import enums.AvatarEnum;
 import enums.RouteEnum;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
